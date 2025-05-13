2b2490cf48f33bab9c7af6b90bf7d1304675019e
54n1h4_21 <154510431+sanihak@users.noreply.github.com>
2025-05-13 15:00:26 +0530

    Add files via upload

---
+++ blurring.py
@@ -0,0 +1,22 @@
+import os
+import cv2
+# img = cv2.imread(os.path.join('.','freelancer.jpg'))
+k_size = 3
+# img_blur = cv2.blur(img,(k_size,k_size))
+# img_gaussian_blur = cv2.GaussianBlur(img, (k_size, k_size), 5)
+# img_median_blur = cv2.medianBlur(img,k_size)
+# cv2.imshow('img',img)
+# cv2.imshow('img_blur',img_blur)
+# cv2.imshow('img_guassian_blur',img_gaussian_blur)
+# cv2.imshow('img_median_blur',img_median_blur)
+# cv2.waitKey(0)
+
+img = cv2.imread(os.path.join('.','cow.jpg'))
+img_blur = cv2.blur(img,(k_size,k_size))
+img_median_blur = cv2.medianBlur(img,k_size)
+cv2.imshow('img_median_blur',img_median_blur)
+cv2.imshow('img_blur',img_blur)
+cv2.imshow('img',img)
+cv2.waitKey(0)
+
+
